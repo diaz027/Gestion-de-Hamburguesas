5a0f3b582e63d5678fd67261f86e5c0cbd1b9b29
Diaz027 <dami27cito@gmail.com>
2024-02-07 15:23:27 -0300

    back

--- src/app/api/Hamburguesas/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/libs/prisma";
-
-export async function GET() {
-  const mercaderia = await prisma.hamburguesas.findMany();
-  return NextResponse.json(mercaderia);
-}
